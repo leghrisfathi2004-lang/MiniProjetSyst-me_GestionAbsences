2c0d733e5e564b242f6cc41109ae4a42563701cf
fathi leghris <leghrisfathi2004@gmail.com>
2025-12-20 20:20:41 +0100

    etudiant page done

--- etudiant/etudiant.js
+++ etudiant/etudiant.js
@@ -1,28 +1,42 @@
-// variables general ---------------------------------------------------
+// variables general ----------------------------------------------------------------
 let total_etd = document.getElementById("etudTotal");
-let actif_etd = 0;
+let actif_etd = document.getElementById("etudActif");
 let inactif_etd = document.getElementById("etudInactif");
 
 let students = JSON.parse(localStorage.getItem("students"));
-if (!idcount) {
-    localStorage.setItem("idcount", 0);
-    }
-
-
-// buttons --------------------------------------------------------------
+let idcount = Number(localStorage.getItem('idcount'));
+// buttons ----------------------------------------------------------------
 let ajoute_etd = document.getElementById("ajoute_etd");
 let annule_ajoute = document.getElementById("annule_etd");
 let save_ajoute = document.getElementById("save_etd");
 let suppreme_etd = document.getElementById("suppreme_etd")
 let modifie_etd = document.getElementById("modifie_etd");
+let btn = document.getElementById("search_btn");
+
 // input ----------------------------------------------------------------
-let search = document.getElementById("search_home");
+let search = document.getElementById("search");
 
 // other ----------------------------------------------------------------
 let table = document.getElementById("table_etd");
 let modal = document.getElementById("modal");
 
-// fonctions ------------------------------------------------------------
+// fonctions ----------------------------------------------------------------
+document.addEventListener( "DOMContentLoaded", () =>
+    displayStudents(students)
+);
+
+btn.onclick = () => {
+    let mot = search.value.trim();
+    if(mot == ""){
+        alert("nothing to search for");
+        displayStudents(students);
+        return;
+    }
+    let result = students.filter(st => ((st.id === Number(mot))||(st.nom === mot)||(st.email === mot)||(st.groupe === mot)));
+    if(result)  displayStudents(result);
+    else alert("cette etudiant n exist pas!")
+};
+
 ajoute_etd.onclick = () => {
     modal.classList.toggle("hidden");
     document.getElementById("modal_mssg").textContent = "ajoute un etudiant: ";
@@ -37,27 +51,32 @@ annule_ajoute.onclick = () => {
 };
 
 save_ajoute.onclick = () => {
+    idcount++;
+    localStorage.setItem('idcount', idcount);
+    idcount = Number(localStorage.getItem('idcount'));
     modal.classList.toggle("hidden");
     let nom_input = document.getElementById("new_nom");
     let email_input = document.getElementById("new_email");
     let groupe_input = document.getElementById("new_grp");
-
-    let students = JSON.parse(localStorage.getItem("students"));
-
     students.push ({
+        id: idcount,
         nom: nom_input.value,
         email: email_input.value,
-        groupe: groupe_input.value,
-        id: idcount++
+        groupe: groupe_input.value
     });
+
     displayStudents(students);
     localStorage.setItem("students" , JSON.stringify(students));
+    students = JSON.parse(localStorage.getItem("students"));
 }
 
 function displayStudents(tab){
 
+    actif_etd.textContent = tab.length;
+    let i = Number( localStorage.getItem('inactifEtd'));
+    inactif_etd.textContent = i;
+    total_etd.textContent = tab.length+i;
     table.innerHTML = "";
-
     tab.forEach(student => {
         const tr = document.createElement("tr");
         tr.className = "text-center";
@@ -79,26 +98,40 @@ function displayStudents(tab){
 }
 
 function deleterow(btn) {
+    if (!localStorage.getItem('inactifEtd')){
+        localStorage.setItem('inactifEtd', '0');
+    }
+    let inactif = Number(localStorage.getItem('inactifEtd'));
+    inactif++;
+    localStorage.setItem('inactifEtd', inactif);
     const row = btn.closest("tr");
     const cells = row.querySelectorAll("td");
-    let students = JSON.parse(localStorage.getItem("students"));
     console.log(cells[0].textContent);
-    students = students.filter(st => (st.id != cells[0].textContent)&&(st.nom != cells[1].textContent));
+    students = students.filter(st => (st.id != cells[0].textContent));
+    displayStudents(students);
     localStorage.setItem("students" , JSON.stringify(students));
-    row.remove();
+    students = JSON.parse(localStorage.getItem("students"));
 }
 
 function editrow(btn) {
     const row = btn.closest("tr");
     const cells = row.querySelectorAll("td");
     document.getElementById("modal_mssg").textContent = "modifie un etudiant: ";
-    document.getElementById("new_nom").value= cells[0].textContent;
-    document.getElementById("new_email").value = cells[1].textContent;
-    document.getElementById("new_grp").value = cells[2].textContent;
+    let id = Number(cells[0].textContent);
+    document.getElementById("new_id").textContent = `etudiant id: ${id}`
+    document.getElementById("new_nom").value= cells[1].textContent;
+    document.getElementById("new_email").value = cells[2].textContent;
+    document.getElementById("new_grp").value = cells[3].textContent;
     modal.classList.toggle("hidden");
-    save_ajoute.onclick = function (){
-           
+    
+    save_ajoute.onclick = () => {
+        let i = students.findIndex(st => st.id == id)
+        students[i].nom = document.getElementById("new_nom").value;
+        students[i].email = document.getElementById("new_email").value;
+        students[i].groupe = document.getElementById("new_grp").value;
+        modal.classList.toggle("hidden");
+        displayStudents(students);
+        localStorage.setItem("students" , JSON.stringify(students));
+        students = JSON.parse(localStorage.getItem("students"));
     }
 }
-
-//still need to (storage the edit and id) and also part about (variables total) and (searche)
